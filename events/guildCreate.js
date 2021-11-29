fdbfc88a2a8e9bb38c7e568f1d4df5583ad0e356
Forcellrus <forcellrus@hotmail.com>
2021-11-29 13:21:57 +0100

    Rare error log fixes

--- events/guildCreate.js
+++ events/guildCreate.js
@@ -2,12 +2,11 @@ const
 	Spam = require('../modules/SpamManager.js'),
 	String = require('../modules/Stringificator.js'),
 	logger = require('../modules/Logger.js'),
-	bot = require('../bot.js'),
-	config = require('../config.json');
+	bot = require('../bot.js');
 
 module.exports = async guild => {
 	if (Spam.blacklistCheck(guild, { leave: true })) return;
 
-	const members = guild.memberCount.toLocaleString(config.log.locale) || 'unknown';
+	const members = guild.memberCount || 'unknown';
 	logger.log(`Joined ${String.guild(guild)} with ${members} members. Servers: ${bot.guilds.cache.size}`);
 };
--- events/guildDelete.js
+++ events/guildDelete.js
@@ -2,12 +2,11 @@ const
 	Spam = require('../modules/SpamManager.js'),
 	String = require('../modules/Stringificator.js'),
 	logger = require('../modules/Logger.js'),
-	bot = require('../bot.js'),
-	config = require('../config.json');
+	bot = require('../bot.js');
 
 module.exports = async guild => {
 	if (Spam.blacklistCheck(guild)) return;
 
-	const members = guild.memberCount.toLocaleString(config.log.locale) || 'unknown';
+	const members = guild.memberCount || 'unknown';
 	logger.log(`Left ${String.guild(guild)} with ${members} members. Servers: ${bot.guilds.cache.size}`);
 };
