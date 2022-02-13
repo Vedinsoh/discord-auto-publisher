6ae261c73d07aaa3177876b64346c101218a73c6
Forcellrus <forcellrus@hotmail.com>
2022-02-13 01:16:18 +0100

    Channel string formatter type changes

--- src/util/stringFormatters.ts
+++ src/util/stringFormatters.ts
@@ -1,11 +1,9 @@
-import { Channel, DMChannel, Guild, PartialDMChannel, TextBasedChannel } from 'discord.js-light';
+import { Guild, GuildChannel } from 'discord.js-light';
 
 export const guildToString = (guild: Guild): string => {
   return `"${guild.name ?? 'unknown guild'}" (${guild.id})`;
 };
 
-export const channelToString = (channel: Channel): string => {
-  const nonDmChannel = channel as Exclude<TextBasedChannel, DMChannel | PartialDMChannel>;
-  if (!nonDmChannel.name) return nonDmChannel.id;
-  return `#${nonDmChannel.name} (${nonDmChannel.id})`;
+export const channelToString = (channel: GuildChannel): string => {
+  return `#${channel.name} (${channel.id})`;
 };
