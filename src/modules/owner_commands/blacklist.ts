30009a1a7ad10ab77707cbdc0b405ef5d8dd92ec
Forcellrus <forcellrus@hotmail.com>
2022-02-18 17:26:41 +0100

    Added loggers for blacklist commands

--- src/modules/owner_commands/blacklist.ts
+++ src/modules/owner_commands/blacklist.ts
@@ -1,8 +1,12 @@
 import { Message, Snowflake } from 'discord.js-light';
 import { Command } from '#structures/Command';
 import Blacklist from '#modules/BlacklistManager';
+import logger from '#util/logger';
 
 export default new Command('blacklist', async ({ channel }: Message, guildId?: Snowflake) => {
   if (!guildId) return channel.send('Please provide server ID.');
-  await Blacklist.add(guildId).then((response) => channel.send(response));
+  await Blacklist.add(guildId).then((response) => {
+    channel.send(response);
+    logger.info(response);
+  });
 });
--- src/modules/owner_commands/unblacklist.ts
+++ src/modules/owner_commands/unblacklist.ts
@@ -1,8 +1,12 @@
 import { Message, Snowflake } from 'discord.js-light';
 import { Command } from '#structures/Command';
 import Blacklist from '#modules/BlacklistManager';
+import logger from '#util/logger';
 
 export default new Command('unblacklist', async ({ channel }: Message, guildId?: Snowflake) => {
   if (!guildId) return channel.send('Please provide server ID.');
-  await Blacklist.remove(guildId).then((response) => channel.send(response));
+  await Blacklist.remove(guildId).then((response) => {
+    channel.send(response);
+    logger.info(response);
+  });
 });
