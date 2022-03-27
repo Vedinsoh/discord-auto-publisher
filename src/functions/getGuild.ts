6799e391a2649f795d6569f37731f9602acb2f83
Forcellrus <forcellrus@hotmail.com>
2022-03-27 22:41:56 +0200

    Fixed type of getGuild eval

--- src/functions/getGuild.ts
+++ src/functions/getGuild.ts
@@ -5,7 +5,7 @@ export default async (guildId: Snowflake): Promise<Guild | undefined> => {
   const evalResult = await client.cluster.broadcastEval(
     (c, { guildId }) => c.guilds.cache.get(guildId),
     { context: { guildId } }
-  );
+  ) as (Guild | undefined)[];
 
   const guilds = evalResult.filter((guild) => guild !== null);
   if (guilds.length) return guilds[0];
