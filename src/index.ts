d76ca910d26163836e5d38e8461de99814b1bac8
Forcellrus <forcellrus@hotmail.com>
2022-04-06 00:23:01 +0200

    Added support for multiple .env environments

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,10 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import path from 'path';
 import { AutoPublisher } from '#structures/ClusterManager';
+
+dotenv.config({
+  path: path.resolve(process.cwd(), `.env.${process.env.NODE_ENV}`),
+})
 
 const manager = new AutoPublisher({
   totalShards: 'auto',
