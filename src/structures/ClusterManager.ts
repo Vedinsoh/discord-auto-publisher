31bef92b41f5f3a80cfa71f6feee24d113559745
Forcellrus <forcellrus@hotmail.com>
2022-04-18 20:16:29 +0200

    Replaced cluster manager options with lib interface

--- src/structures/ClusterManager.ts
+++ src/structures/ClusterManager.ts
@@ -1,26 +1,10 @@
-import { ClusterManagerMode, keepAliveOptions, Manager } from 'discord-hybrid-sharding';
+import { Manager, ManagerOptions } from 'discord-hybrid-sharding';
 import { getFiles } from '#util/fileUtils';
 import { minToMs } from '#util/timeConverters';
 import logger from '#util/logger';
 
 export class AutoPublisher extends Manager {
-  // Inferred options from discord-hybrid-sharding's Manager constructor
-  /* eslint-disable @typescript-eslint/ban-types */
-  constructor(options?: {
-    totalShards?: number | 'auto';
-    totalClusters?: number | 'auto';
-    shardsPerClusters?: number;
-    shardList?: 'auto' | number[][];
-    mode?: ClusterManagerMode;
-    respawn?: boolean;
-    shardArgs?: string[];
-    token?: string;
-    execArgv?: string[];
-    keepAlive?: keepAliveOptions;
-    queue?: { auto?: boolean };
-    clusterData?: Object;
-    clusterOptions?: Object;
-  }) {
+  constructor(options?: ManagerOptions) {
     super(getFiles('../AutoPublisher{.ts,.js}')[0], options);
   }
 
