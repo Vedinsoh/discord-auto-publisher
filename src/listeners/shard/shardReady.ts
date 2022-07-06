33ad930f963a7a50ecdf2fa3f89188803a6926b3
Forcellrus <forcellrus@hotmail.com>
2022-07-06 18:14:40 +0200

    Fixed startup time logging

--- src/listeners/shard/shardReady.ts
+++ src/listeners/shard/shardReady.ts
@@ -1,6 +1,11 @@
+import client from '#client';
 import { Event } from '#structures/Event';
+import { msToSec } from '#util/timeConverters';
 import logger from '#util/logger';
 
 export default new Event('shardReady', async (id: number) => {
-  logger.info(`[Shard #${id}] Ready!`);
+  const logPrefix = `[SHARD ${id}]`;
+  const logInfo = (message: string) => logger.info(`${logPrefix} ${message}`);
+
+  logInfo(`Ready! Startup time: ${msToSec(Date.now() - client.startedAt).toFixed(2)}s`);
 });
