b3061de61816e8cf1fbf86ece677bec64c67759b
Forcellrus <forcellrus@hotmail.com>
2022-11-06 22:01:37 +0100

    chore: removed excess env vars

--- src/types/environment.d.ts
+++ src/types/environment.d.ts
@@ -2,7 +2,5 @@ declare namespace NodeJS {
   export interface ProcessEnv {
     BOT_TOKEN: string;
     REDIS_PORT: string;
-    NODE_ENV: 'development' | 'production';
-    BASE_DIR: 'src' | 'dist';
   }
 }
