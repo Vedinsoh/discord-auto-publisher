8ce751570216d5e3a99d8f8fd20e917996d76fe4
Vedinsoh <16418183+Vedinsoh@users.noreply.github.com>
2023-03-23 21:34:54 +0100

    fix: removed rate limit data global flag

--- src/crosspost/gatewayQueueFilter.ts
+++ src/crosspost/gatewayQueueFilter.ts
@@ -2,7 +2,7 @@ import type { RateLimitData } from 'discord.js';
 import routes from '#constants/routes';
 
 const gatewayQueueFilter = (data: RateLimitData): boolean => {
-  return data.method.toUpperCase() === 'POST' && routes.crosspost === data.route && !data.global;
+  return data.method.toUpperCase() === 'POST' && routes.crosspost === data.route;
 };
 
 export default gatewayQueueFilter;
