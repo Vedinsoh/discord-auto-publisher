8c6bf8578011677144db5104718436a96d216fa3
Vedinsoh <16418183+Vedinsoh@users.noreply.github.com>
2023-05-03 14:40:48 +0200

    script: added local images cleanup to compose dev

--- scripts/compose-dev.sh
+++ scripts/compose-dev.sh
@@ -5,4 +5,4 @@ COMPOSE_FILES="-f docker-compose.yml -f docker-compose.development.yml"
 docker compose $COMPOSE_FILES down
 docker compose $COMPOSE_FILES build
 docker compose $COMPOSE_FILES run --rm --service-ports bot
-docker compose $COMPOSE_FILES down
+docker compose $COMPOSE_FILES down --rmi local
