cef30e9a9d737fefbf2002ffac120d422b20a9a4
Vedinsoh <16418183+Vedinsoh@users.noreply.github.com>
2023-07-09 13:30:57 +0200

    perf: removed permission checks & caches

--- src/AutoPublisher.ts
+++ src/AutoPublisher.ts
@@ -1,7 +1,12 @@
 import { getInfo } from 'discord-hybrid-sharding';
-import { GatewayIntentBits as IntentBits, Options, Partials } from 'discord.js';
+import { type CacheWithLimitsOptions, GatewayIntentBits as IntentBits, Options, Partials } from 'discord.js';
 import config from '#config';
 import AutoPublisherClient from '#structures/Client';
+
+type CacheOptions = CacheWithLimitsOptions & {
+  PermissionOverwriteManager: 0;
+  RoleManager: 0;
+};
 
 const client = new AutoPublisherClient({
   makeCache: Options.cacheWithLimits({
@@ -13,25 +18,24 @@ const client = new AutoPublisherClient({
     GuildEmojiManager: 0,
     GuildForumThreadManager: 0,
     GuildInviteManager: 0,
-    GuildMemberManager: {
-      maxSize: 1,
-      keepOverLimit: (member) => member.id === member.client.user.id,
-    },
+    GuildMemberManager: 0,
     GuildScheduledEventManager: 0,
     GuildStickerManager: 0,
     GuildTextThreadManager: 0,
     MessageManager: 0,
+    PermissionOverwriteManager: 0,
     PresenceManager: 0,
     ReactionManager: 0,
     ReactionUserManager: 0,
+    RoleManager: 0,
     StageInstanceManager: 0,
     ThreadManager: 0,
     ThreadMemberManager: 0,
     UserManager: 0,
     VoiceStateManager: 0,
-  }),
+  } as CacheOptions),
   intents: [IntentBits.Guilds, IntentBits.GuildMessages, IntentBits.DirectMessages, IntentBits.MessageContent],
-  partials: [Partials.Channel, Partials.GuildMember],
+  partials: [Partials.Channel],
   shards: getInfo().SHARD_LIST,
   shardCount: getInfo().TOTAL_SHARDS,
   rest: {
