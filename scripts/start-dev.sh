0cad9f31e224e77205ebd50579ab154b5b4a329c
Vedinsoh <16418183+Vedinsoh@users.noreply.github.com>
2024-07-12 17:21:25 +0200

    fix(WIP): local development compose scripts

--- scripts/start-dev.sh
+++ scripts/start-dev.sh
@@ -3,6 +3,6 @@
 COMPOSE_FILES="-f docker-compose.dev.yml"
 
 docker compose $COMPOSE_FILES down
-docker compose $COMPOSE_FILES build &&
-docker compose $COMPOSE_FILES run --rm --service-ports bot
+docker compose $COMPOSE_FILES up &&
+docker compose $COMPOSE_FILES run --rm
 docker compose $COMPOSE_FILES down --rmi local
