a61c4eaa63c5e17174f770ee474d53886685831f
Vedinsoh <16418183+Vedinsoh@users.noreply.github.com>
2024-07-31 21:53:31 +0200

    config: env defaults adjustments

--- services/bot/src/utils/config.ts
+++ services/bot/src/utils/config.ts
@@ -1,13 +1,13 @@
 import 'dotenv/config';
-import { cleanEnv, num, port, str, testOnly, url } from 'envalid';
+import { cleanEnv, num, port, str, url } from 'envalid';
 import { levels as loggerLevels } from 'pino';
 
 export const env = cleanEnv(process.env, {
   // Common
-  NODE_ENV: str({ devDefault: testOnly('test'), choices: ['development', 'production', 'test'] }),
+  NODE_ENV: str({ default: 'development', choices: ['development', 'production', 'test'] }),
   LOGGER_LEVEL: str({ default: 'info', choices: Object.values(loggerLevels.labels) }),
   REDIS_URI: url({ default: 'redis://rest-cache:6379' }),
-  DISCORD_TOKEN: str({ devDefault: testOnly('') }),
+  DISCORD_TOKEN: str({ default: '' }),
 
   // Bot
   BOT_ADMINS: str({ default: '' }),
--- services/rest/src/utils/config.ts
+++ services/rest/src/utils/config.ts
@@ -1,17 +1,17 @@
 import 'dotenv/config';
 
-import { cleanEnv, host, port, str, testOnly, url } from 'envalid';
+import { cleanEnv, host, port, str, url } from 'envalid';
 import { levels as loggerLevels } from 'pino';
 
 export const env = cleanEnv(process.env, {
   // Common
-  NODE_ENV: str({ devDefault: testOnly('test'), choices: ['development', 'production', 'test'] }),
+  NODE_ENV: str({ default: 'development', choices: ['development', 'production', 'test'] }),
   LOGGER_LEVEL: str({ default: 'info', choices: Object.values(loggerLevels.labels) }),
   REDIS_URI: url({ default: 'redis://rest-cache:6379' }),
   MONGO_URI: url({ default: 'mongodb://rest-db:27017' }),
-  DISCORD_TOKEN: str({ devDefault: testOnly('') }),
+  DISCORD_TOKEN: str({ default: '' }),
 
   // REST
-  REST_HOST: host({ devDefault: 'localhost' }),
+  REST_HOST: host({ default: 'localhost' }),
   REST_PORT: port({ devDefault: 3000 }),
 });
