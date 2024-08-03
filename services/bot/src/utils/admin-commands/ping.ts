4fc39e7103127dda954723bdd41cf97faaf0ec07
Vedinsoh <16418183+Vedinsoh@users.noreply.github.com>
2024-08-03 11:01:09 +0200

    refactor: ping admin command

--- services/bot/src/utils/admin-commands/ping.ts
+++ services/bot/src/utils/admin-commands/ping.ts
@@ -3,23 +3,9 @@ import AdminCommand from '#structures/AdminCommand';
 import { CommandNames } from '#types/AdminCommandTypes';
 
 export default new AdminCommand(CommandNames.PING, async ({ channel }) => {
-  const runs = [1, 2, 3];
-  const pings: number[] = [];
+  const perfStart = performance.now();
+  await channel.send(`Measuring...`);
+  const perfEnd = performance.now();
 
-  await Promise.all(
-    runs.map(async (run) => {
-      const start = performance.now();
-      await channel.send(`Ping ${run}...`).then(() => {
-        pings.push(performance.now() - start);
-      });
-    })
-  );
-  pings.sort((a, b) => a - b);
-
-  const parseMs = (ms: number) => `${Math.ceil(ms)}ms`;
-
-  const average = (pings[0] + pings[1] + pings[2]) / 3;
-  channel.send(
-    `Low: ${parseMs(pings[0])}\nMid: ${parseMs(pings[1])}\nHigh: ${parseMs(pings[2])}\nAverage: ${parseMs(average)}`
-  );
+  channel.send(`**Ping:** ${Math.ceil(perfEnd - perfStart)}ms`);
 });
