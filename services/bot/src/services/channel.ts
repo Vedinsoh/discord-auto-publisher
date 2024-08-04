42332a1aba476c28e1c86c7ad8b416b63cc570f9
Vedinsoh <16418183+Vedinsoh@users.noreply.github.com>
2024-08-04 12:03:14 +0200

    docs: channel service

--- services/bot/src/services/channel.ts
+++ services/bot/src/services/channel.ts
@@ -1,5 +1,10 @@
 import type { Message } from 'discord.js';
 
+/**
+ * Fetches complete channel data
+ * @param channel The channel to fetch
+ * @returns Channel data
+ */
 const fetch = async (channel: Message['channel']) => {
   // Get the channel data if it's partial
   if (channel.partial) {
