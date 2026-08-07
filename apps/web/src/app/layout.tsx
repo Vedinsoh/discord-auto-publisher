902e551f04d690a22cdfc98ccc1df31376373430
acehox <16418183+acehox@users.noreply.github.com>
2026-08-07 12:54:36 +0200

    refactor: add smooth scroll behavior to HTML layout

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -30,7 +30,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" data-scroll-behavior="smooth">
       <body
         className={`${geistSans.variable} ${geistMono.variable} ${geistSans.className} antialiased`}
       >
