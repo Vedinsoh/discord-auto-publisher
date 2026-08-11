bcad5c8ffa24d9ff6b2e92cc75d98a6f147f25ad
acehox <16418183+acehox@users.noreply.github.com>
2026-08-11 13:45:31 +0200

    refactor: update URL regex pattern for improved matching and adjust Redis memory policy in docker-compose

--- packages/utils/src/regex.ts
+++ packages/utils/src/regex.ts
@@ -1,4 +1,5 @@
-export const url = /\b((https?:\/\/)?(www\.)?[a-zA-Z0-9.-]+\.[a-zA-Z]{2,})(\/\S*)?\b/;
+export const url =
+  /\b(?:https?:\/\/|www\.)[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*\.[a-zA-Z]{2,}|\b[a-zA-Z0-9-]+\.[a-zA-Z]{2,}\/\S/i;
 
 export const RegExPatterns = {
   url,
